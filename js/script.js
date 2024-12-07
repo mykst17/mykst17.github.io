753a307a8a515b8e2cef17ae9c9021b509e1d4d2
mykst17 <mykst.88@gmail.com>
2024-12-07 01:52:55 -0500

    g

--- js/script.js
+++ js/script.js
@@ -37,3 +37,81 @@ document.addEventListener("scroll", function () {
     }
   });
 });
+
+// ポップアップを表示する要素を取得
+const popup = document.getElementById("popup");
+const popupImg = document.getElementById("popup-img");
+const closePopupButton = document.getElementById("close-popup");
+
+// ポップアップ2を表示する要素を取得
+const popup2 = document.getElementById("popup-2");
+const popup2Img = document.getElementById("popup-img-2");
+const closePopup2Button = document.getElementById("close-popup-2");
+
+// ポップアップ3を表示する要素を取得
+const popup3 = document.getElementById("popup-3");
+const popup3Img = document.getElementById("popup-img-3");
+const closePopup3Button = document.getElementById("close-popup-3");
+
+// 各ホットスポットの画像を定義
+const hotspotImages = {
+  "hotspot-1": "imgs/floral-motif.png", // Floral Motifの画像
+  "hotspot-2": "imgs/flowering-branches.png", // Flowering Branchesの画像
+  "hotspot-3": "imgs/rim.png", // Rimの画像
+};
+
+const hotspots2 = {
+  "hotspot-4": "imgs/undecorated-interior.png",
+  "hotspot-5": "imgs/flared-rim.png",
+  "hotspot-6": "imgs/geometric-shapes.png",
+};
+
+const hotspots = {
+  "hotspot-7": "imgs/musicians.png",
+  "hotspot-8": "imgs/flowers.png",
+  "hotspot-9": "imgs/dancers.png",
+};
+
+// ポップアップを表示する関数
+function showPopup(popupElement, popupImgElement, imageUrl) {
+  // 他のポップアップが表示されていたら非表示にする
+  [popup, popup2, popup3].forEach((popup) => {
+    popup.style.display = "none";
+  });
+
+  // 対象のポップアップを表示
+  popupImgElement.src = imageUrl;
+  popupElement.style.display = "flex";
+}
+
+// ホットスポットのクリックイベントを設定
+document.querySelectorAll(".Hotspot").forEach((hotspot) => {
+  hotspot.addEventListener("click", (event) => {
+    const hotspotId = event.target.closest(".Hotspot").getAttribute("slot");
+    let imageUrl = null;
+
+    if (hotspotImages[hotspotId]) {
+      imageUrl = hotspotImages[hotspotId];
+      showPopup(popup, popupImg, imageUrl);
+    } else if (hotspots2[hotspotId]) {
+      imageUrl = hotspots2[hotspotId];
+      showPopup(popup2, popup2Img, imageUrl);
+    } else if (hotspots[hotspotId]) {
+      imageUrl = hotspots[hotspotId];
+      showPopup(popup3, popup3Img, imageUrl);
+    }
+  });
+});
+
+// ポップアップを閉じるイベント
+closePopupButton.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+closePopup2Button.addEventListener("click", () => {
+  popup2.style.display = "none";
+});
+
+closePopup3Button.addEventListener("click", () => {
+  popup3.style.display = "none";
+});
